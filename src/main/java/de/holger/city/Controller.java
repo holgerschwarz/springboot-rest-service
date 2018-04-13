0a70fe0cba830206fbdd48d85438a3036767b153
Holger Schwarz <holger.schwarz@vorsorge-leben.de>
2018-04-13 17:13:40 +0200

    init Rest get

---
+++ src/main/java/de/holger/city/Controller.java
@@ -0,0 +1,38 @@
+package de.holger.city;
+
+
+import de.holger.city.model.CityDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+
+
+@RequestMapping(path="/v1/cities")
+public class Controller {
+
+    @RequestMapping( method = RequestMethod.GET)
+    List<CityDto> getAllCities() {
+        CityDto cityDto = new CityDto();
+        cityDto.setCityname("Monheim");
+        cityDto.setPopulation(40000L);
+        List<CityDto> cities= new ArrayList<>();
+        cities.add(cityDto);
+
+        return cities;
+    }
+
+    @RequestMapping(path = "/{name}",method = RequestMethod.GET)
+    CityDto getCity(@PathVariable String name) {
+        CityDto cityDto = new CityDto();
+        cityDto.setCityname(name);
+        cityDto.setPopulation(40000L);
+
+        return cityDto;
+    }
+
+
+
+}
